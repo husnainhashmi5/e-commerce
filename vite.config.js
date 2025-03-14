307b6650ecf69fa591f4e486990d1b20da4bedb3
HMTAhir327 <tahir4work@gmail.com>
2025-03-14 13:17:58 +0300

    dynamic import test

--- vite.config.js
+++ vite.config.js
@@ -19,3 +19,9 @@ export default defineConfig({
     }
   }
 });
+
+const images = import.meta.glob("/src/assets/*", { eager: true });
+
+const getImageUrl = (imagePath) => {
+  return images[imagePath]?.default || "";
+};
